ed4a888dbfa501c164c2257e17bf1235eae05de9
mashrur-rumon <mh.rumon10@gmail.com>
2020-11-03 19:23:13 +0600

    name input field

---
+++ LAB_TASK_3.1_PHP/TASK_3_INPUT_FIELD/name.php
@@ -0,0 +1,37 @@
+<?php
+	
+	$data = "";
+
+	if(isset($_REQUEST['submit'])){
+		$name = $_REQUEST['name'];
+		
+		if($name == ""){
+			$data = "";
+		}else{
+			$data = $name;
+		}
+	}
+?>
+
+
+<html>
+<head>
+	<title>HTML form-Name</title>
+</head>
+<body>	
+    	
+    <form method="post">
+        
+        <fieldset style="font-family:Calibri;width:200px;height:100px">
+            
+         <legend><b>Name</b></legend>
+         
+         <input type="text" name="name" value="<?=$data?>"> <hr>
+        <input style="font-family:Calibri" type="submit" name="submit" value="Submit">
+        </fieldset>
+    
+        
+    </form>
+		
+</body>
+</html>
