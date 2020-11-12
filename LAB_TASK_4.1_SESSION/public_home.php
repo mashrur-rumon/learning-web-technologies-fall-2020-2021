f34e2513b7f7cf1768050a974072f9b052fb20ba
mashrur-rumon <mh.rumon10@gmail.com>
2020-11-13 01:11:14 +0600

    public home session

---
+++ LAB_TASK_4.1_SESSION/public_home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>XCompany - Home</title>
+</head>
+<centre>
+<body>   
+        
+        <table cellspacing="0" border="1" width="800px" align=center>
+            
+            <tr height="50px">
+                
+                <td width=200px>
+                    
+                    <a href="public_home.php"><img src="logo.png" alt="Logo"></a>                      
+                    
+                </td>
+                
+                <td align="right">
+                    
+                <pre>    <a href="public_home.php">Home</a>|<a href="login.php">Login</a>|<a href="registration.php">Registration</a>   </pre>
+                    
+                </td>
+       
+            </tr>
+            
+            <tr>
+                
+                <td colspan="2" height=180px><h2>Welcome to xCompany</h2></td>
+                
+            </tr>
+            
+            <tr>
+                
+                <td colspan="2" align="center"><p>Copyright &#169; 2017</p></td>
+                
+            </tr>
+            
+        </table>
+        
+    
+    
+</body>
+</html>
