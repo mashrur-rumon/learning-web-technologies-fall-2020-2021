f44a1ff6b32553964294b2e982ee291fe105f9e6
mashrur-rumon <mh.rumon10@gmail.com>
2020-12-14 10:14:16 +0600

    final commit

--- FINAL_LAB TASK_AJAX_4.1/view/registration.php
+++ FINAL_LAB TASK_AJAX_4.1/view/registration.php
@@ -1,37 +1,50 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>REGISTRATION</title>
+    <meta charset="UTF-8">
+    <title>Registration</title>
 </head>
 <body>
-<center>
-	<form>
-		<table border="0" cellspacing="0" cellpadding="5">
-			<tr>
-				<td>
-					<fieldset>
-						<legend><b>REGISTRATION</b></legend>
-						Id<br />
-						<input type="text" name="id" /><br />
-						Password<br />
-						<input type="password" name="id"/><br />
-						Confirm Password<br />
-						<input type="password"name="id"/><br />
-                        Name<br />
-						<input type="text" name="id"/><br />
-                        Email<br />
-						<input type="email" name="id"/><br />
-                        User Type<i>User Admin)</i><br />
-						<select name="User Type" >
-				          <option value=""selected>Admin</option>
-				          <option value="">Client</option>			
-			            </select> 								
-						<hr />
-						<input type="submit" name="submit" value="Register"> <a href="../index.php">Login</a>						
-					</fieldset>
-				</td>
-			</tr>
-		</table>
-	</form>
-</center>
+    
+    <br><br><br><br><br><br>
+    
+    <center>
+        
+        <fieldset style="width:450px;">
+            
+            <legend><h1>Registration</h1></legend>
+            
+            <form>
+                
+                Id <br>
+                <input type="text" name="id"><br>
+                Password <br>
+                <input type="password" name="password"><br>
+                Confirm password <br>
+                <input type="password" name="cpassword"><br>
+                Name <br>
+                <input type="text" name="name"><br>
+                Email <br>
+                <input type="email" name="email"><br>           
+                User Type [User/Admin] <br>   
+                <select name="userType" >
+				    <option value="User">User</option>
+				    <option value="Admin">Admin</option>	
+			    </select>
+               
+               <hr>
+               
+               <input type="submit" name="submit" value="Register" onclick="registration()"> <a href="../index.php">Login</a> <br>
+                
+            </form>
+            
+        </fieldset>
+        
+        <div id="result"></div>
+        
+        <script type="text/javascript" src="../script/script.js"></script>
+        
+    </center>
+    
 </body>
 </html>
---
+++ FINAL_LAB TASK_AJAX_4.1/view/userHome.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Home</title>
+</head>
+<body>
+   
+   
+    <br><br><br><br>
+    
+    
+    <center>
+        
+        <h1>Welcome Anne</h1>
+        
+        <a href="userProfile.php">Profile</a><br>
+        <a href="changePassword.php">Change Password</a><br>
+        <a href="logout.php">Logout</a>
+        
+    </center>
+    
+</body>
+</html>
