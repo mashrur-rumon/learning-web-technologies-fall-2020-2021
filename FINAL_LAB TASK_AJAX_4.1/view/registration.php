7538f045570118d6ecc831cd19373b29482b2163
mashrur-rumon <mh.rumon10@gmail.com>
2020-12-14 09:12:16 +0600

    first commit

---
+++ FINAL_LAB TASK_AJAX_4.1/view/registration.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+	<title>REGISTRATION</title>
+</head>
+<body>
+<center>
+	<form>
+		<table border="0" cellspacing="0" cellpadding="5">
+			<tr>
+				<td>
+					<fieldset>
+						<legend><b>REGISTRATION</b></legend>
+						Id<br />
+						<input type="text" name="id" /><br />
+						Password<br />
+						<input type="password" name="id"/><br />
+						Confirm Password<br />
+						<input type="password"name="id"/><br />
+                        Name<br />
+						<input type="text" name="id"/><br />
+                        Email<br />
+						<input type="email" name="id"/><br />
+                        User Type<i>User Admin)</i><br />
+						<select name="User Type" >
+				          <option value=""selected>Admin</option>
+				          <option value="">Client</option>			
+			            </select> 								
+						<hr />
+						<input type="submit" name="submit" value="Register"> <a href="../index.php">Login</a>						
+					</fieldset>
+				</td>
+			</tr>
+		</table>
+	</form>
+</center>
+</body>
+</html>
